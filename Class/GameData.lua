0972ed7ee7516a551b209bac72d7077a13537698
Frank Williames <dev.frankw@gmail.com>
2023-09-23 16:48:33 -0400

    criando a variavel TempRecord

--- Class/GameData.lua
+++ Class/GameData.lua
@@ -3,8 +3,10 @@ local class = require("libs.middleclass")
 local GameData = class("GameData") --- @class GameData
 
     function GameData:initialize(record)
+        
         ListOfBullets = {}
         Score = 0
+        TempRecord = 0
         
         if record ~= nil then
             if record > Record then
