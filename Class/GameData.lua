2e1d05d49e9ad9bbb7a069a6242c62e3b7eb0f3a
Frank Williames <dev.frankw@gmail.com>
2023-09-23 01:42:50 -0400

    add singleton

---
+++ Class/GameData.lua
@@ -0,0 +1,19 @@
+local class = require("libs.middleclass")
+
+local GameData = class("GameData") --- @class GameData
+
+    function GameData:initialize(record)
+        ListOfBullets = {}
+        Score = 0
+        
+        if record ~= nil then
+            if record > Record then
+                Record = record
+            end
+        else
+            Record = 0
+        end
+
+    end
+
+return GameData
